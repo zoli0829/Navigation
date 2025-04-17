ce5a732f26292431c86436df16b6f571b621ca78
zoli0829 <zoli0829@gmail.com>
2025-04-17 12:50:26 +0100

    Initial commit

---
+++ Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Navigation
+//
+//  Created by Zoltan Vegh on 17/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Zoltan Vegh on 17/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
